f3370c660d85c5bb95915405c6297169ee90af97
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2024-04-08 11:48:48 +0200

    Add component template file

---
+++ template.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "CSS_FILE_NAME"
+
+export default function COMPONENT_NAME({ CHILDREN_PROPS }) {
+function COMPONENT_NAME({ CHILDREN_PROPS }) {
+  return (
+    <div>
+      <div>COMPONENT_NAME</div>
+      { CHILDREN_PROPS }
+    </div>
+  );
+}
+
+export default COMPONENT_NAME;
